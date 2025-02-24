3f0fcae7d77415c6cf2af8be0e60c0cd1f86c0be
josserden <josserden@gmail.com>
2025-02-24 18:45:01 +0200

    feat: update Home component title for clarity

--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -5,7 +5,7 @@ import { Typography } from '@/components/ui/typography';
 export const Home: FC = () => {
   return (
     <Typography className='text-4xl font-bold' color='primary' variant='h1'>
-      Page
+      Home
     </Typography>
   );
 };
